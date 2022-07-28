a8127433ae70c7aa086bd89c72e1fc40081bcbdd
ashot08 <albertguzeev@yandex.ru>
2022-07-28 06:48:13 +0300

    add video post type

--- wp-content/plugins/courses_dashboard_2/functions.php
+++ wp-content/plugins/courses_dashboard_2/functions.php
@@ -36,6 +36,60 @@ require_once __DIR__ . '/models/Director.php';
 require_once __DIR__ . '/ajax/ajax.php';
 require_once __DIR__ . '/libs/vendor/autoload.php';
 
+
+
+// Регистрация типов данных
+//--------------------------------------------------------------------
+
+add_action( 'init', 'register_post_types' );
+
+function register_post_types(){
+
+    register_post_type( 'post_type_name', [
+        'label'  => 'cd__video',
+        'labels' => [
+            'name'               => 'Видео', // основное название для типа записи
+            'singular_name'      => 'Видео', // название для одной записи этого типа
+            'add_new'            => 'Добавить Видео', // для добавления новой записи
+            'add_new_item'       => 'Добавление Видео', // заголовка у вновь создаваемой записи в админ-панели.
+            'edit_item'          => 'Редактирование Видео', // для редактирования типа записи
+            'new_item'           => 'Новое Видео', // текст новой записи
+            'view_item'          => 'Смотреть Видео', // для просмотра записи этого типа.
+            'search_items'       => 'Искать Видео', // для поиска по этим типам записи
+            'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
+            'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
+            'parent_item_colon'  => '', // для родителей (у древовидных типов)
+            'menu_name'          => 'Видео', // название меню
+        ],
+        'description'         => '',
+        'public'              => true,
+        // 'publicly_queryable'  => null, // зависит от public
+        // 'exclude_from_search' => null, // зависит от public
+        // 'show_ui'             => null, // зависит от public
+        // 'show_in_nav_menus'   => null, // зависит от public
+        'show_in_menu'        => null, // показывать ли в меню адмнки
+        // 'show_in_admin_bar'   => null, // зависит от show_in_menu
+        'show_in_rest'        => null, // добавить в REST API. C WP 4.7
+        'rest_base'           => null, // $post_type. C WP 4.7
+        'menu_position'       => null,
+        'menu_icon'           => null,
+        //'capability_type'   => 'post',
+        //'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
+        //'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
+        'hierarchical'        => false,
+        'supports'            => [ 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats' ], // 'title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'
+        'taxonomies'          => ['wpm-category'],
+        'has_archive'         => false,
+        'rewrite'             => true,
+        'query_var'           => true,
+    ] );
+
+}
+
+//--------------------------------------------------------------------
+
+
+
 // Подключаем скрипту courses_dashboard_ajax атрибут type="module"
 //--------------------------------------------------------------------
 add_filter('script_loader_tag', 'add_type_attribute' , 10, 3);
